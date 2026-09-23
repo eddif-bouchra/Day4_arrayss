0eb5937b652e8a4048bd309437205df605f6b07d
eddif-bouchra <bouchraeddif@gmail.com>
2026-09-23 09:00:25 +0000

    solve:Fusionner deux tableaux sans doublons

--- exercice34.js
+++ exercice34.js
@@ -1,6 +1,5 @@
-let tab = [1, 2, 3]
-let arr = [3,2, 4, 5]
-let uniqueTab = []
+let tab = [1, 2, 6, 3]
+let arr = [3, 2, 4, 5]
 for (let i = 0; i < arr.length; i++) {
     let kayn = false
     for (let j = 0; j < tab.length; j++) {
@@ -12,4 +11,5 @@ for (let i = 0; i < arr.length; i++) {
     if (!kayn)
         tab.push(arr[i])
 }
+tab.sort()
 console.log(tab)
