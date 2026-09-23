d355d537913260a7bd79e218884c0e9e8c3303f5
eddif-bouchra <bouchraeddif@gmail.com>
2026-09-23 09:51:56 +0000

    solve: Plus longue suite de valeurs identiques

--- exercice36.js
+++ exercice36.js
@@ -1,11 +1,7 @@
 let arr = [1, 1, 2, 2, 2, 3, 1, 1, 1, 1]
-
-
-
 let count = 1
 let maxCount = 0
 let head = arr[0]
-
 for (let i = 1; i < arr.length; i++) {
     if (arr[i] === head) {
         count++
@@ -14,8 +10,6 @@ for (let i = 1; i < arr.length; i++) {
 
         }
     }
-
-
     else {
         count = 1
         head = arr[i]
